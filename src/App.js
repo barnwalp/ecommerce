3fecf2e0f96006680eabc252fb87b99cc82603ac
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-09-20 21:17:58 +0530

    feat/added tailwind in project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-6 py-6">
+			<h1 className="text-3xl">Hello world</h1>
+			<p>Dolor iusto laboriosam numquam fuga nemo Alias officia eveniet dolores sunt non Cumque quod in ut natus facere. Rem voluptates dolorem adipisci illum necessitatibus. Rem officiis accusantium soluta nostrum debitis.</p>
     </div>
   );
 }
