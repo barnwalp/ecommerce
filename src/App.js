ca80e16b6f799be6d9d0054eab0579a0ad4c719a
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-09-27 10:16:04 +0530

    feat/Using Outlet to create fixed navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Home from './Routes/home/home.component.jsx';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
-function App() {
+const Navigation = () => {
+	return (
+		<div>
+			<div>
+				<h1 className="mx-auto mt-3 text-3xl max-w-max">E-Commerce</h1>
+			</div>
+			<Outlet />
+		</div>
+	)
+}
+
+const Shop = () => {
+	return (
+		<div>
+			<h2>Show Now</h2>
+		</div>
+	)
+}
+
+const App = () => {
   return (
 		<Routes>
-			<Route path='/home' element={<Home />} />
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
+			</Route>
 		</Routes>
   );
 }
--- src/Routes/home/home.component.jsx
+++ src/Routes/home/home.component.jsx
@@ -4,7 +4,6 @@ import Directory from '../../components/directory/directory.component.jsx';
 function Home() {
   return (
     <div className="px-6 py-6">
-			<h1 className="mx-auto mb-6 text-3xl max-w-max">E-Commerce</h1>
 			<Directory categories={categories} />
     </div>
   );
