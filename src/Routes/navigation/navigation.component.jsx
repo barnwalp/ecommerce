28e6d3dcf14b90ba651c6760ef8e5ed156c0a85a
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-09-28 20:32:16 +0530

    feat/adding navigtion bar using Outlet and Link

--- src/Routes/navigation/navigation.component.jsx
+++ src/Routes/navigation/navigation.component.jsx
@@ -1,13 +1,25 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as Crown } from '../../assets/crown.svg';
 
 const Navigation = () => {
 	return (
-		<div>
-			<div>
-				<h1 className="mx-auto mt-3 text-3xl max-w-max">E-Commerce</h1>
+		<Fragment>
+			<div className="mx-8 mt-3 text-3xl flex justify-between">
+				<Link to="/">
+					<Crown />
+				</Link>
+				<div>	
+					<Link className="ml-4 text-gray-900 hover:text-gray-500" to="/shop">
+						Shop
+					</Link>
+					<Link className="ml-4 text-gray-900 hover:text-gray-500" to="/contact">
+						Contact
+					</Link>
+				</div>
 			</div>
 			<Outlet />
-		</div>
+		</Fragment>
 	)
 }
 
